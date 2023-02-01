d069da64e05d05dc809dfbf42329bfc00cccddd3
SimonStrebel <s.strebel95@gmail.com>
2023-02-01 11:20:49 +0100

    Initial commit

---
+++ Plot_Resultate_Anlage.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import yaml
+from matplotlib import pyplot as plt
+import matplotlib
+import mpl_axes_aligner
+import numpy as np
+
+# Einstellungen das alle Daten sichtbar wenn DF angezeigt werden soll:
+pd.set_option('display.max_columns', 150)
+pd.set_option('display.width', 2000)
+
+# Pfad Datei und ablage Plot importieren:
+with open(f'Config_Resultat_Plot.yaml', 'r') as Inputs:
+    inputs = yaml.safe_load(Inputs)
+
+Pfad_Datei = inputs['Pfad_Datei_Resultate']
+Pfad_Plot = inputs['Pfad_Plot_Poster']
+
+# CSV Files einlesen
+df = pd.read_csv(f'{Pfad_Datei}')
+# print(f'{df} \n')
+
+# Daten günstigste Li-Systeme für Plot in eigenes DF
+df_Plot = df.nsmallest(4, 'Anlagekosten mit Li-Batterie [CHF]', keep='all')
+
+# Nach PV-Leistung sortieren
+df_Plot.sort_values(by=['PV-Leistungen [W]'], inplace=True)
+print(f'{df_Plot} \n')
+
+# Schriftgrösse einstellen
+matplotlib.rcParams.update({'font.size': 14})
+
+# Plot der Systemkosten und Batteriegrösse in Abhängigkeit der PV-Leistung (Lithium)
+fig, ax1 = plt.subplots(figsize=(10, 8))
+ax1_sek = ax1.twinx()
+# fig.suptitle(f'Systemkosten Off-Grid-Anlage', fontweight='bold')
+ax1.plot(df_Plot['PV-Leistungen [W]'], df_Plot['Anlagekosten mit Li-Batterie [CHF]'], linewidth=2.5, marker='x', markersize=8, color='#C20078')
+ax1_sek.plot(df_Plot['PV-Leistungen [W]'], df_Plot['Batteriegrösse Li [Wh]'], linewidth=2.5, marker='x', markersize=8, color='#069AF3')
+
+# Reihenfolge bei überschreitenden Linien vorgeben und sicherstellen, dass nichts ausgeblendet wird:
+ax1.set_zorder(2)
+ax1_sek.set_zorder(1)
+ax1.set_frame_on(False)
+
+# Darstellungen Plot:
+ax1.tick_params(axis='x', labelrotation=0)
+ax1.set_xlabel('PV-Leistung [Wp]', labelpad=10)
+ax1.set_xticks(np.arange(250, 540, 20))
+
+ax1.set_ylabel('Systemkosten [CHF]', color='#C20078', labelpad=5)
+ax1.tick_params(axis='y', colors='#C20078')
+ax1.set_yticks(np.arange(800, 1020, 20))
+mpl_axes_aligner.shift.yaxis(ax=ax1, org=800 , pos=0)
+#ax1.grid(linewidth=0.75, zorder=-10)
+
+ax1_sek.set_ylabel('Batteriegrösse [Wh]', color='#069AF3', labelpad=15)
+ax1_sek.tick_params(axis='y', colors='#069AF3')
+ax1_sek.set_yticks(np.arange(440, 760, 20))
+mpl_axes_aligner.shift.yaxis(ax=ax1_sek, org=440 , pos=0)
+#ax1_sek.grid(linewidth=0.75, zorder=-10)
+
+# Ausgabe und speichern:
+plt.tight_layout()
+plt.show()
+speichern_als = f'{Pfad_Plot}/Plot_Li_besteAnlagen.jpg'
+fig.savefig(speichern_als, dpi=500)
+plt.clf()
+plt.close()
---
+++ Plot_Verluste.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import yaml
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+import os
+import matplotlib
+
+# Schriftgrösse einstellen
+matplotlib.rcParams.update({'font.size': 14})
+
+# Informationen importieren
+with open(f'Config_Verlust_Plot.yaml', 'r') as Inputs:
+    inputs = yaml.safe_load(Inputs)
+
+Pfad_Datei = inputs['Pfad Datei']
+Pfad_Resultate = inputs['Pfad Resultate']
+Dateiname = inputs['Dateiname']
+MPPT_Wirkungsgrad = inputs['MPPT_Wirkungsgrad']/100
+MPPT_Eigenverbrauch = inputs['MPPT_Eigenverbrauch']
+MPPT_Trackingverlust = inputs['MPPT_Trackingverlust']
+
+# CSV-Datei einlesen
+df_Daten = pd.read_csv(f"{Pfad_Datei}", index_col='Datum und Zeit')
+df_Daten.index = pd.to_datetime(df_Daten.index, format='%Y-%m-%d %H:%M:%S')
+
+# Spaltenname PV-Leistung und Batteriestand bestimmen, damit diese Spalte genutzt werden kann
+df_pvleistung = df_Daten.filter(regex='Leistung PV')
+for spalte_pvleistung in df_pvleistung.columns:
+    pvleistung = spalte_pvleistung
+
+df_batteriestand = df_Daten.filter(regex='Batteriestand')
+for spalte_batteriestand in df_batteriestand.columns:
+    batteriestand = spalte_batteriestand
+    # Ausserdem Batteriegrösse speichern:
+    batteriegrösse = df_Daten.filter(regex='Batteriestand').max()[0]
+
+# Siehe Projektbericht, Kapitel "limitierender Parameter" zur Erläuterung, welcher Verlust wie berechnet wird
+# Verluste PV-Überschuss
+df_Daten.loc[(df_Daten[batteriestand] == batteriegrösse) & (df_Daten[pvleistung] > 0), 'Verlust PV-Überschuss [Wh]'] = df_Daten[pvleistung]*0.25 - df_Daten['Energiebedarf 1/4h Lastbetrieb [Wh]']
+df_Daten.loc[(df_Daten[batteriestand] < batteriegrösse) | (df_Daten[pvleistung] == 0), 'Verlust PV-Überschuss [Wh]'] = 0
+
+# Verluste Wirkungsgrad
+df_Daten['Verlust Wirkungsgrad [Wh]'] =  ((df_Daten[pvleistung]/MPPT_Wirkungsgrad)-df_Daten[pvleistung])*0.25
+
+# Verluste Tracking
+df_Daten.loc[(df_Daten['P_PV [W/Wp]'] > 0), 'Trackingverlust [Wh]'] =  MPPT_Trackingverlust*0.25
+df_Daten.loc[(df_Daten['P_PV [W/Wp]'] == 0), 'Trackingverlust [Wh]'] =  0
+
+# Verluste Eigenverbrauch
+df_Daten.loc[(df_Daten[pvleistung] > 0) | (df_Daten[batteriestand] > 0), 'Eigenverbrauchsverlust [Wh]'] = MPPT_Eigenverbrauch*0.25
+df_Daten.loc[(df_Daten[pvleistung] == 0) & (df_Daten[batteriestand] == 0), 'Eigenverbrauchsverlust [Wh]'] = 0
+
+# Bei PV-Überschuss, werden die anderen Verluste auf 0 gesetzt
+df_Daten.loc[(df_Daten['Verlust PV-Überschuss [Wh]'] > 0), 'Verlust Wirkungsgrad [Wh]'] = 0
+df_Daten.loc[(df_Daten['Verlust PV-Überschuss [Wh]'] > 0), 'Trackingverlust [Wh]'] = 0
+df_Daten.loc[(df_Daten['Verlust PV-Überschuss [Wh]'] > 0), 'Eigenverbrauchsverlust [Wh]'] = 0
+
+# Gesamtverluste der Parameter berechnen [kWh]
+Verluste_Wirkungsgrad_ges = df_Daten['Verlust Wirkungsgrad [Wh]'].sum()/1000
+Verluste_Tracking_ges = df_Daten['Trackingverlust [Wh]'].sum()/1000
+Verluste_Eigenverbrauch_ges = df_Daten['Eigenverbrauchsverlust [Wh]'].sum()/1000
+Verluste_PV_Überschuss_ges = df_Daten['Verlust PV-Überschuss [Wh]'].sum()/1000
+
+# PV-Leistung bestimmen, damit gesamte Input-Energie berechnet werden kann
+PV_Leistung = ((df_Daten.filter(regex='Leistung PV').max()[0]/MPPT_Wirkungsgrad)+MPPT_Trackingverlust)/df_Daten['P_PV [W/Wp]'].max()
+PV_Leistung = round(PV_Leistung, 2)
+print(f'Die PV-Leistung der ausgewerteten Anlage beträgt: {PV_Leistung} W')
+
+# Energie ins System berechnen
+df_Daten['Energie IN [Wh]'] = df_Daten['P_PV [W/Wp]']*PV_Leistung*0.25
+Energie_IN = df_Daten['Energie IN [Wh]'].sum()/1000
+
+# Genutzte Energie Last (ohne Eigenverbrauch, der separat abgezogen wird)
+df_Daten.loc[(df_Daten['Status Last'] == 1), 'Genutzte Energie [Wh]'] = df_Daten['Energiebedarf Last [Wh]']
+df_Daten.loc[(df_Daten['Status Last'] == 0), 'Genutzte Energie [Wh]'] = 0
+Genutzte_Energie = df_Daten['Genutzte Energie [Wh]'].sum()/1000
+
+# Berechnung der weiteren Verluste (dies sind z.B. der Akku-Wirkungsgrad und die Selbsentladung)
+# Input abzüglich aller Verluste und der genutzten Energie [kWh]
+Weitere_Verluste = Energie_IN - Verluste_Wirkungsgrad_ges - Verluste_Tracking_ges - Verluste_Eigenverbrauch_ges - Verluste_PV_Überschuss_ges - Genutzte_Energie
+
+# Pie-Chart erstellen
+names = f'Wirkungsgrad \n{Verluste_Wirkungsgrad_ges:.2f} kWh', f'Tracking \n{Verluste_Tracking_ges:.2f} kWh', f'Eigenverbrauch \n{Verluste_Eigenverbrauch_ges:.2f} kWh'
+sizes = [Verluste_Wirkungsgrad_ges, Verluste_Tracking_ges, Verluste_Eigenverbrauch_ges]
+colors = ['#4F6272', '#B7C3F3', '#DD7596']
+
+fig, ax1 = plt.subplots(figsize=(12, 6))
+#fig.suptitle(f'Mein Titel', fontweight='bold')
+patches, texts, autotexts = ax1.pie(sizes, labels=names, labeldistance=1.15, wedgeprops={'linewidth' : 5, 'edgecolor' : 'white'}, colors=colors, autopct='%1.2f%%', pctdistance=0.7, startangle=90)
+for autotext in autotexts:
+    autotext.set_color('white')
+ax1.axis('equal')  # Ensures that pie is drawn as a circle
+
+# Zusatzinfos in Textblock
+uberschrift = f' Alle Verluste'
+plt.figtext(0.78, 0.78, uberschrift, fontsize=14, weight='bold', horizontalalignment ="left", verticalalignment ="bottom")
+
+text = f' Wirkungsgrad: \n {Verluste_Wirkungsgrad_ges:.2f} kWh \n\n Tracking: \n {Verluste_Tracking_ges:.2f} kWh \n\n Eigenverbrauch: \n {Verluste_Eigenverbrauch_ges:.2f} kWh \n\n PV-Überschuss: \n {Verluste_PV_Überschuss_ges:.2f} kWh \n\n Weitere Verluste: \n {Weitere_Verluste:.2f} kWh \n\n Genutzte Energie: \n {Genutzte_Energie:.2f} kWh'
+plt.figtext(0.78, 0.76, text, fontsize=14, style='italic', horizontalalignment ="left", verticalalignment ="top")
+
+# Donut erstellen (weisser Kreis im Zentrum)
+centre_circle = plt.Circle((0, 0), 0.4, color='white')
+plt.gcf().gca().add_artist(centre_circle)
+
+# Plot zeigen und speichern
+plt.tight_layout()
+plt.show()
+speichern_unter = Pfad_Resultate
+fig.savefig(f'{Pfad_Resultate}/{Dateiname}.jpg', bbox_inches='tight', dpi=500)
