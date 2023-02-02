3035a3fa35af02b34b64996418c23b3f75ba8e6d
SimonStrebel <s.strebel95@gmail.com>
2023-02-02 10:00:07 +0100

    Plot Übersicht Energie

--- Plot_Resultate_Anlagen.py
+++ Plot_Resultate_Anlagen.py
@@ -14,14 +14,14 @@ with open(f'Config_Resultat_Plot.yaml', 'r') as Inputs:
     inputs = yaml.safe_load(Inputs)
 
 Pfad_Datei = inputs['Pfad_Datei_Resultate']
-Pfad_Plot = inputs['Pfad_Plot_Poster']
+Pfad_Plot = inputs['Pfad_Plot']
 
 # CSV Files einlesen
 df = pd.read_csv(f'{Pfad_Datei}')
 # print(f'{df} \n')
 
 # Daten günstigste Li-Systeme für Plot in eigenes DF
-df_Plot = df.nsmallest(4, 'Anlagekosten mit Li-Batterie [CHF]', keep='all')
+df_Plot = df.nsmallest(6, 'Anlagekosten mit Li-Batterie [CHF]', keep='all')
 
 # Nach PV-Leistung sortieren
 df_Plot.sort_values(by=['PV-Leistungen [W]'], inplace=True)
@@ -31,7 +31,7 @@ print(f'{df_Plot} \n')
 matplotlib.rcParams.update({'font.size': 14})
 
 # Plot der Systemkosten und Batteriegrösse in Abhängigkeit der PV-Leistung (Lithium)
-fig, ax1 = plt.subplots(figsize=(10, 8))
+fig, ax1 = plt.subplots(figsize=(10, 6))
 ax1_sek = ax1.twinx()
 # fig.suptitle(f'Systemkosten Off-Grid-Anlage', fontweight='bold')
 ax1.plot(df_Plot['PV-Leistungen [W]'], df_Plot['Anlagekosten mit Li-Batterie [CHF]'], linewidth=2.5, marker='x', markersize=8, color='#C20078')
@@ -45,18 +45,20 @@ ax1.set_frame_on(False)
 # Darstellungen Plot:
 ax1.tick_params(axis='x', labelrotation=0)
 ax1.set_xlabel('PV-Leistung [Wp]', labelpad=10)
-ax1.set_xticks(np.arange(250, 540, 20))
+ax1.set_xticks(np.arange(120, 320, 20))
 
 ax1.set_ylabel('Systemkosten [CHF]', color='#C20078', labelpad=5)
 ax1.tick_params(axis='y', colors='#C20078')
-ax1.set_yticks(np.arange(800, 1020, 20))
-mpl_axes_aligner.shift.yaxis(ax=ax1, org=800 , pos=0)
+y0_ax1 = 400
+ax1.set_yticks(np.arange(y0_ax1, 660, 20))
+mpl_axes_aligner.shift.yaxis(ax=ax1, org=y0_ax1 , pos=0)
 #ax1.grid(linewidth=0.75, zorder=-10)
 
 ax1_sek.set_ylabel('Batteriegrösse [Wh]', color='#069AF3', labelpad=15)
 ax1_sek.tick_params(axis='y', colors='#069AF3')
-ax1_sek.set_yticks(np.arange(440, 760, 20))
-mpl_axes_aligner.shift.yaxis(ax=ax1_sek, org=440 , pos=0)
+y0_axsek = 150
+ax1_sek.set_yticks(np.arange(y0_axsek, 700, 50))
+mpl_axes_aligner.shift.yaxis(ax=ax1_sek, org=y0_axsek , pos=0)
 #ax1_sek.grid(linewidth=0.75, zorder=-10)
 
 # Ausgabe und speichern:
